5369b5ba4996f8da9f99148e9c77f41475c40390
Jeany <72220793+Mr-Jeany@users.noreply.github.com>
2024-03-31 12:13:41 +0300

    Update Program.cs

--- 4-1/Program.cs
+++ 4-1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _4_1
 {
@@ -9,14 +9,36 @@ namespace _4_1
             ArraySimple<int> intArray = new ArraySimple<int>();
             intArray.Print();
             intArray.Add(5);
+            intArray.Print();
             intArray.Remove(2);
+            intArray.Print();
+            intArray.Reverse();
+            intArray.Print();
             intArray.Sort();
-            intArray.Count();
-            intArray.CountWithCondition((x) => x > 6);
-            intArray.CheckIfOneIsTrue((x) => x == 5);
-            intArray.CheckIfAllAreTrue((x) => x < 3);
-            intArray.Contains(10);
+            intArray.Print();
+            Console.WriteLine(intArray.Count());
+            Console.WriteLine(intArray.CountWithCondition((x) => x > 6));
+            Console.WriteLine(intArray.CheckIfOneIsTrue((x) => x == 5));
+            Console.WriteLine(intArray.CheckIfAllAreTrue((x) => x < 3));
+            Console.WriteLine(intArray.Contains(10));
             intArray.MakeForEachElement((x) => Console.Write(x*x + " "));
+
+            ArraySimple<string> stringArray = new ArraySimple<string>();
+            stringArray.Print();
+            stringArray.Add("name");
+            stringArray.Print();
+            stringArray.Remove(2);
+            stringArray.Print();
+            stringArray.Reverse();
+            stringArray.Print();
+            stringArray.Sort();
+            stringArray.Print();
+            Console.WriteLine(stringArray.Count());
+            Console.WriteLine(stringArray.CountWithCondition((x) => x.Length > 3));
+            Console.WriteLine(stringArray.CheckIfOneIsTrue((x) => x.Length == 4));
+            Console.WriteLine(stringArray.CheckIfAllAreTrue((x) => x.Length >= 2));
+            Console.WriteLine(stringArray.Contains("hello"));
+            stringArray.MakeForEachElement((x) => Console.Write(x + " "));
         }
     }
 }
